65e36696cf02f73456a0c74284d6f7776ec6fef2
Andrey Lebedev <lebedancer@gmail.com>
2017-01-30 10:43:52 +0300

    navigation throw browserHistory

--- src/components/RulesList/RuleListItem/RuleListItem.js
+++ src/components/RulesList/RuleListItem/RuleListItem.js
@@ -1,17 +1,28 @@
 import React, {PropTypes} from 'react';
+import { browserHistory } from 'react-router';
 
 import MdCloseLink from '../../common/MdCloseLink';
 import style from './style.css';
 
 
 const RuleListItem = ({ data })=>{
+
+    function _onClick(e) {
+        e.preventDefault();
+
+        const href = _getHref(data.Id);
+        browserHistory.push(href);
+    }
+
+    function _getHref(id) {
+        return `/rule/${id}`
+    }
+
     return (
-        <li className={style.item}><a href={_getHref(data.Id)}>{data.Name}</a><MdCloseLink /></li>
+        <li className={style.item}><a href={_getHref(data.Id)} onClick={_onClick}>{data.Name}</a><MdCloseLink /></li>
     )
 };
 
-function _getHref(id) {
-    return `/rule/${id}`
-}
+
 
 export default RuleListItem;
